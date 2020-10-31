d0fec70963a92095ebe3b71c7f8d8fe6eed5e928
Shuijing725 <minnie725@outlook.com>
2020-10-31 17:48:49 -0500

    remove import Agent, add for loop for multiple episodes

--- decision_rule.py
+++ decision_rule.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from agent import Agent
+# from agent import Agent
 
 
 class DecisionRule():
--- main.py
+++ main.py
@@ -53,29 +53,31 @@ def main(args):
     env.unwrapped.viewer.window.on_key_press = key_press
     env.unwrapped.viewer.window.on_key_release = key_release
     global do_user_action, user_action
-    state = env.reset()
-    total_reward = 0
-    decision_rule = DecisionRule()
+    for i_episode in range(args.num_episodes):
+        state = env.reset()
+        total_reward = 0
+        decision_rule = DecisionRule()
 
-    while True:
-        chosen_action = decision_rule.get_action(state, user_action)
-        next_state, reward, done, info = env.step(chosen_action)
-#             if reward != 0:
-#                 print("reward : ", reward)
+        while True:
+            chosen_action = decision_rule.get_action(state, user_action)
+            next_state, reward, done, info = env.step(chosen_action)
+    #             if reward != 0:
+    #                 print("reward : ", reward)
 
-        total_reward += reward
-        env.render()
-        if done:
-            break
-        state = next_state
-        time.sleep(0.05)
-
+            total_reward += reward
+            env.render()
+            if done:
+                break
+            state = next_state
+            time.sleep(0.05)
+        print('Episode', i_episode, ': reward =', total_reward)
     env.close()
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='LunarLander-v2 Discrete')
-
+    parser.add_argument('--num_episodes', type=int, default = 2000,
+                        help='number of episodes for training')
     args = parser.parse_args()
     main(args)
 
